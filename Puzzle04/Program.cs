4d27a60cf82fbe5ce13a97f4b3314a96e33b3116
Ilya Opyakin <i.opyakin@outlook.com>
2021-12-05 16:16:57 +0100

    Minor refactoring

--- Puzzle04/Program.cs
+++ Puzzle04/Program.cs
@@ -13,8 +13,8 @@ namespace MyApp // Note: actual namespace depends on the project name.
         private readonly int instanceId;
         private bool bWin;
 
-        private readonly int[,] CardNumbers = new int[CARD_SIZE, CARD_SIZE];
-        private readonly bool[,] CardChecks = new bool[CARD_SIZE, CARD_SIZE];
+        private readonly int[,]  CardNumbers = new int [CARD_SIZE, CARD_SIZE];
+        private readonly bool[,] CardChecks  = new bool[CARD_SIZE, CARD_SIZE];
 
         public BingoCard(int[,] CardData)
         {
@@ -24,46 +24,33 @@ namespace MyApp // Note: actual namespace depends on the project name.
                 for (int c = 0; c < CARD_SIZE; c++)
                 {
                     CardNumbers[r, c] = CardData[r, c];
-                    CardChecks[r, c] = false;
+                    CardChecks [r, c] = false;
                 }
         }
         public bool Winner { get { return bWin; } }
         private bool setWin {  set { bWin = true; } }
         public int GetId { get { return instanceId; } }
         public static int CardSize { get { return CARD_SIZE; } }
-        public int CheckRows(int r)
+        public bool isFiveInRowOrCol(int row, int col)
         {
-            int res = 0;
-            for (int c = 0; c < CARD_SIZE; c++)
-                if (CardChecks[r, c] == true)
-                    res++;
+            int nNumbersInRow = 0;
+            int nNumbersInCol = 0;
+            for (int i = 0; i < CARD_SIZE; i++)
+            {
+                if (CardChecks[row, i] == true) nNumbersInRow++;
+                if (CardChecks[i, col] == true) nNumbersInCol++;
+            }
 
-            if (res == CARD_SIZE)
-                {
-                    setWin = true;
-                    return r;
-                }
-            
-            return -1;
+            if (nNumbersInRow == CARD_SIZE || nNumbersInCol == CARD_SIZE)
+            {
+                setWin = true;
+                return true;
+            }
+
+            return false;
         }
 
-        public int CheckColumns(int c)
-        {
-            int res = 0;
-            for (int r = 0; r < CARD_SIZE; r++)
-                if (CardChecks[r, c] == true)
-                    res++;
-
-                if (res == CARD_SIZE)
-                {
-                    setWin = true;
-                    return c;
-                }
-
-            return -1;
-        }
-
-        public bool MarkNumer(int nDrawNumber)
+        public bool isWinner(int nDrawNumber)
         {
             for (int r = 0; r < CARD_SIZE; r++)
                 for (int c = 0; c < CARD_SIZE; c++)
@@ -71,13 +58,7 @@ namespace MyApp // Note: actual namespace depends on the project name.
                     if (CardNumbers[r, c] == nDrawNumber)
                     { 
                         CardChecks[r, c] = true;
-                        int nWinnerRow = CheckRows(r);
-                        int nWinnerCol = CheckColumns(c);
-
-                        if (nWinnerRow >= 0 || nWinnerCol >= 0)
-                            return true;
-                        else
-                            return false;
+                        return isFiveInRowOrCol(r,c);
                     }
                 }
 
@@ -102,93 +83,85 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         // Answer for Data_p.txt  Part 1: 25023     Part 2: 2634
         static readonly string filePath = @".\..\..\..\Data_p.txt";
-        static int[] nDrawNumbers;
+        static int[]? nDrawNumbers;
         static List<BingoCard> BingoCardsList = new List<BingoCard>();
         public static void Main(string[] args)
         {
             // parsing the input file
             List<string> InputData = new();
             using (StreamReader file = new(filePath))
+            {
+                GetDrawNumbers(file.ReadLine());
                 while (!file.EndOfStream)
                 {
                     string? line = file.ReadLine();
-                    if (line != null)
-                        InputData.Add(line);
+                    InputData.Add(line);
                 }
+            }
 
-            GetDrawNumbers(InputData);
-            // Remove Draw Numbers line and empty line after it
-            InputData.RemoveAt(0);
-            InputData.RemoveAt(0);
-            // Added empty line at the end of input as terminator
-            InputData.Add("");
             CreateBingoCards(InputData);
+            PlayTheBingo();
+        }
 
+        private static void PlayTheBingo()
+        {
             int nBingoCardId = 0;
-            foreach(int num in nDrawNumbers)
-                foreach(BingoCard bingoCard in BingoCardsList)
+            foreach (int num in nDrawNumbers)
+                foreach (BingoCard bingoCard in BingoCardsList)
+                {
+                    // ignore the card which has won already
+                    if (bingoCard.Winner)
+                        continue;
+
+                    if (bingoCard.isWinner(num))
                     {
-                        if (bingoCard.Winner)
-                            continue;
+                        int nSumm = bingoCard.GetSumOfUnmarkedNumbers();
+                        // Showing the first card and the last card
+                        if (nBingoCardId == 0 || nBingoCardId == BingoCardsList.Count - 1)
+                            Console.WriteLine("Bingo! Card Id: {3,2:0}   Summ: {0,4:0}   Last Num: {1,2:0}   Answer: {2,6:0}", nSumm, num, nSumm * num, bingoCard.GetId);
 
-                        bool Winner = bingoCard.MarkNumer(num);
-                        //int nWinnerRow = bingoCard.CheckRows();
-                        //int nWinnerCol = bingoCard.CheckColumns();
-
-                        if(Winner)
-                            {
-                                int nSumm = bingoCard.GetSumOfUnmarkedNumbers();
-                                // Showing the first card and the last card
-                                if(nBingoCardId == 0 || nBingoCardId == BingoCardsList.Count - 1)
-                                    Console.WriteLine("Bingo! Card Id: {3,4:0}   Summ: {0,4:0}    Last Num: {1,2:0}   Answer: {2,8:0}", nSumm, num, nSumm * num, bingoCard.GetId);
-
-                                nBingoCardId++;
-                            }        
+                        nBingoCardId++;
                     }
-
+                }
         }
 
         // Parsing the Input Data and populating BingoCardsList with the instances of the BingoCard
         private static void CreateBingoCards(List<string> inputData)
         {
-            
             int nRow = 0;
             int[,] CardNumbers = new int[BingoCard.CardSize, BingoCard.CardSize];
             foreach (string cardRow in inputData)
             {
-
                 if (cardRow == "")
-                {
-                    BingoCard bingoCard = new BingoCard(CardNumbers);
-                    BingoCardsList.Add(bingoCard);
-                    nRow = 0;
                     continue;
-                }
 
                 // Replacing double space (before single digit number) by single space
                 // Removing the leading space
                 string sFancyCardRow = cardRow.Replace("  ", " ").Trim();
 
                 string[] sRow = sFancyCardRow.Split(" ");
-                for(int c = 0; c < BingoCard.CardSize; c++)
-                {
-                    CardNumbers[nRow, c] = Int32.Parse(sRow[c]);
-                }
-                nRow++;
-            }
 
+                for(int c = 0; c < BingoCard.CardSize; c++)
+                    CardNumbers[nRow, c] = Int32.Parse(sRow[c]);
+
+                nRow++;
+
+                if(nRow == BingoCard.CardSize)
+                {
+                    BingoCardsList.Add(new BingoCard(CardNumbers));
+                    nRow = 0;
+                }
+            }
         }
 
-        private static void GetDrawNumbers(List<string> inputData)
+        private static void GetDrawNumbers(string sDrawNumbers)
         {
-            string[] sDrawNumbers = inputData.First().Split(",");
-            nDrawNumbers = new int[sDrawNumbers.Length];
+            string[] sDrawNumbersArray = sDrawNumbers.Split(",");
+            nDrawNumbers = new int[sDrawNumbersArray.Length];
 
             int nIndex = 0;
-            foreach (string s in sDrawNumbers)
-            {
+            foreach (string s in sDrawNumbersArray)
                 nDrawNumbers[nIndex++] = int.Parse(s);
-            }
         }
     }
 }
