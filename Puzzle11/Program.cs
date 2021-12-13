40f56d94ae223b4ab187e2725ad51f3b40ccc93f
Ilya Opyakin <i.opyakin@outlook.com>
2021-12-13 21:55:51 +0100

    Final version of Puzzle 11

--- Puzzle11/Program.cs
+++ Puzzle11/Program.cs
@@ -3,52 +3,54 @@ namespace MyApp
 {
     public class Program
     {
-        // Answers for Data_p.txt  Part 1:      Part 2: 
-        static readonly string filePath = @".\..\..\..\Data_p.txt";
+        // Answers for Data_p.txt  Part 1:1723      Part 2:327 
+        static readonly string filePath = @".\..\..\..\Data_t.txt";
         static readonly int X = 10;
         static readonly int Y = 10;
-
-        static int nFlashes = 0;
+        
         static int[,] OctopusMap = new int[X, Y];
         public static void Main(string[] args)
         {
+            int nFlashes = 0;
             ParsingInputData();
-            int nResOne = 0;
-            int nResTwo = 0;
-
-            for (int nStep = 0; nStep < 100 || nResTwo == 0; nStep++)
+            // 1000 is a random limit to avoid infinite loop
+            for (int nStep = 0; nStep < 1000; nStep++)
             {
                 if (nStep == 100)
-                    nResOne = nFlashes;
+                    Console.WriteLine("Part one: {0, 10:0}", nFlashes);
 
-                if (CheckBigFlash() && nResTwo == 0)
-                    nResTwo = nStep;
-
-                IncreaseEnergyByOne();
-                InitiateFlash();
-            }
-
-            Console.WriteLine("Part one: {0, 10:0}", nResOne);
-            Console.WriteLine("Part one: {0, 10:0}", nResTwo);
-        }
-        private static void InitiateFlash()
-        {
-            bool bChainReaction = false;
-
-            for (int x = 0; x < X; x++)
-                for (int y = 0; y < Y; y++)
+                if (CheckBigFlash())
                 {
-                    if (OctopusMap[x, y] > 9)
-                    {
-                        OctopusMap[x, y] = -1000;
-                        IncreaseEnergyOfAdjusted(x, y);
-                        bChainReaction = true;
-                    }
+                    Console.WriteLine("Part one: {0, 10:0}", nStep);
+                    break;
                 }
 
-            if (bChainReaction)
-                InitiateFlash();
+                nFlashes+=RunOneCycle();
+            }
+        }
+        private static int RunOneCycle()
+        {
+            // increasing energy level of each octopus by 1
+            for (int x = 0; x < X; x++)
+                for (int y = 0; y < Y; y++)
+                    OctopusMap[x, y]++;
 
+            // initiate chain reaction
+            bool bChainReaction = true;
+            while (bChainReaction)
+            {
+                bChainReaction = false;
+                for (int x = 0; x < X; x++)
+                    for (int y = 0; y < Y; y++)
+                        if (OctopusMap[x, y] > 9)
+                        {
+                            OctopusMap[x, y] = -1000; // -1000 is octopus who flashed this cycle
+                            IncreaseEnergyOfNearestOctopus(x, y);
+                            bChainReaction = true;
+                        }
+            }
+            // return flashed octopus to 0 level of energy
+            int nFlashes = 0;
             for (int x = 0; x < X; x++)
                 for (int y = 0; y < Y; y++)
                     if (OctopusMap[x, y] < 0)
@@ -56,18 +58,9 @@ namespace MyApp
                         OctopusMap[x, y] = 0;
                         nFlashes++;
                     }
+            
+            return nFlashes;
         }
-        private static void ShowTheMap()
-        {
-            for (int y = 0; y < Y; y++)
-            {
-                for (int x = 0; x < X; x++)
-                    Console.Write(OctopusMap[x,y].ToString());
-
-                Console.WriteLine();
-            }
-        }
-
         private static bool CheckBigFlash()
         {
             int n = 0;
@@ -77,22 +70,13 @@ namespace MyApp
 
             return n==100 ? true : false;
         }
-
-        private static void IncreaseEnergyByOne()
-        {
-            for (int x = 0; x < X; x++)
-                for (int y = 0; y < Y; y++)
-                    OctopusMap[x, y]++;
-        }
-        private static void IncreaseEnergyOfAdjusted(int x, int y)
+        private static void IncreaseEnergyOfNearestOctopus(int x, int y)
         {
             int[,] d = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 }, { -1, -1 }, { -1, 1 }, { 1, -1 }, { 1, 1 } };
 
             for (int i = 0; i < 8; i++)
                 if (isValidCoordinates(x + d[i, 0], y + d[i, 1]))
-                {
                     OctopusMap[x + d[i, 0], y + d[i, 1]]++;
-                }
         }
         private static void ParsingInputData()
         {
