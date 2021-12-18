921d4c9833f4e2e7fd5897fb388fced6d62feb5b
Ilya Opyakin <i.opyakin@outlook.com>
2021-12-18 14:13:23 +0100

    Part one completed

--- Puzzle14/Program.cs
+++ Puzzle14/Program.cs
@@ -4,26 +4,63 @@
     {
         // Answers for Data_p.txt  Part 1:      Part 2: 
         static readonly string filePath = @".\..\..\..\Data_t.txt";
-        static List<int> InputData = new List<int>();
+        static string PTemplate;
+        static Dictionary<string, char> Rules = new Dictionary<string,char>();
         public static void Main(string[] args)
         {
             ParsingInputData();
-            Console.WriteLine("Part one: {0, 10:0}", 1);
+            string s = PTemplate;
+            for (int i = 0; i < 10; i++)
+            {
+                s = EncodePolymer(s);
+            }
+
+            int nMax = 0;
+            int nMin = int.MaxValue;    
+            foreach(char value in Rules.Values.Distinct())
+            {
+                int n = s.Count(n => n == value);
+                if(n > nMax) nMax = n;
+                if(n < nMin) nMin = n;
+            }
+
+            Console.WriteLine("Part one: {0, 10:0}", nMax - nMin);
             Console.WriteLine("Part one: {0, 10:0}", 2);
         }
 
+        private static string EncodePolymer(string s)
+        {
+            string sNewPolymer = s;
+            int delta = 1;
+            for(int i = 0; i < s.Length - 1; i++)
+            {
+                string Key = String.Concat(s[i],s[i + 1]);
+
+                if (Rules.ContainsKey(Key))
+                {
+                    sNewPolymer = sNewPolymer.Insert(i + delta, Rules[Key].ToString());
+                    delta++;
+                }
+
+            }
+
+            return sNewPolymer;
+        }
 
         private static void ParsingInputData()
         {
             using (StreamReader file = new(filePath))
+            {
+                PTemplate = file.ReadLine();
+                            file.ReadLine(); // Empty line
                 while (!file.EndOfStream)
                 {
                     string line = file.ReadLine();
-                    string[] parts = line.Split(",");
-
-                    foreach (string s in parts)
-                        InputData.Add(int.Parse(s));
+                    string[] parts = line.Split(" -> ");
+                    Rules.Add(parts[0], parts[1][0]);
                 }
+
+            }
         }
     }
 }
