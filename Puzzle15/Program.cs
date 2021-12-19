74bbe8053226820326555be455ffe0612c41930b
Ilya Opyakin <i.opyakin@outlook.com>
2021-12-19 17:11:13 +0100

    Part two completed

--- Puzzle15/Program.cs
+++ Puzzle15/Program.cs
@@ -2,12 +2,13 @@
 {
     public class Program
     {
-        // Answers for Data_p.txt  Part 1: 415     Part 2: 
+        // Answers for Data_p.txt  Part 1: 415     Part 2: 2864
         static readonly string filePath = @".\..\..\..\Data_p.txt";
         static List<int> InputData = new List<int>();
 
-        static readonly int X = 100;
-        static readonly int Y = 100;
+        // !!! change the X & Y. for the test X=Y=10 , for prod X=Y=100
+        static int X = 100;
+        static int Y = 100;
 
         static int[,] Map = new int[X, Y];
         static int[,] InitialMap = new int[X, Y];
@@ -15,29 +16,54 @@
         public static void Main(string[] args)
         {
             ParsingInputData();
-
+            Console.WriteLine("Part one: {0, 10:0}", FindSafeRoute());
+            
+            ScanWholeCave();
+            Console.WriteLine("Part Two: {0, 10:0}", FindSafeRoute());
+        }
+        private static int FindSafeRoute()
+        {
             // Recursive Wave function is too expensive, run it in iterations
             int n = 10;
             while (--n > 0)
-            {
                 for (int x = 0; x < X; x++)
                     for (int y = 0; y < Y; y++)
                         Wave(x, y);
-            }
 
-            int nRes = Map[X - 1, Y - 1] - Map[0, 0];
-
-            Console.WriteLine("Part one: {0, 10:0}", nRes);
-            Console.WriteLine("Part one: {0, 10:0}", 2);
+            return Map[X - 1, Y - 1] - Map[0, 0];
         }
+        private static void ScanWholeCave()
+        {
+            int nOldDimension = X;
+            X *= 5;
+            Y *= 5;
 
+            Map = new int[X, Y];
+            VisitedMap = new bool[X, Y];
+
+            int nRiskLevelDelta = 0;
+
+            for(int x = 0; x < X; x++)
+                for(int y = 0;y < Y; y++)
+                {
+                    nRiskLevelDelta = y / nOldDimension + x / nOldDimension;
+                    int nNewValue = InitialMap[x % nOldDimension, y % nOldDimension] + nRiskLevelDelta;
+
+                    if (nNewValue > 9)
+                        nNewValue = nNewValue % 9;
+                    
+                    Map[x,y] = nNewValue;
+                }
+
+            InitialMap = new int[X, Y];
+            for (int x = 0; x < X; x++)
+                for (int y = 0; y < Y; y++)
+                    InitialMap[x, y] = Map[x, y];
+        }
         private static void Wave(int x, int y)
         {
-            //ShowMap();
             VisitedMap[x, y] = true;
-
             int[,] d = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
-
             int nCost = Map[x, y];
 
             for (int i = 0; i < 4; i++)
@@ -45,16 +71,10 @@
                 int nXnew = x + d[i, 0];
                 int nYnew = y + d[i, 1];
                 if (isValidCoordinates(nXnew, nYnew))
-                {
                     if (VisitedMap[nXnew, nYnew] == false || InitialMap[nXnew, nYnew] + nCost < Map[nXnew, nYnew])
                         Map[nXnew, nYnew] = InitialMap[nXnew, nYnew] + nCost;
-
-                }
             }
         }
-
-
-
         private static void ParsingInputData()
         {
             int y = 0;
@@ -71,25 +91,6 @@
                     }
                     y++;
                 }
-        }
-        private static void ShowMap()
-        {
-            for (int y = 0; y < Y; y++)
-            {
-
-                string text = "";
-                for (int x = 0; x < X; x++)
-                {
-                    Console.Write("{0, 4:0}", Map[x, y]);
-                    text += Map[x, y].ToString() + "  ";
-                }
-
-                Console.WriteLine();
-                //using StreamWriter file = new(@"e:\WriteLines2.txt", append: true);
-                //    file.WriteLineAsync(text);
-                Console.WriteLine();
-            }
-            Console.WriteLine();
         }
         public static bool isValidCoordinates(int x, int y)
         {
