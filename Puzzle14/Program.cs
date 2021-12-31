41febd09148bf6d82ab2e09a00d50a97e2b7dab6
Ilya Opyakin <i.opyakin@outlook.com>
2021-12-31 16:54:57 +0100

    Puzzle 14 - some minor updates

--- Puzzle14/Program.cs
+++ Puzzle14/Program.cs
@@ -4,17 +4,15 @@
     {
         // Answers for Data_p.txt  Part 1: 2899     Part 2: 3528317079545
         static readonly string filePath = @".\..\..\..\Data_t.txt";
-        static Dictionary<string, char> Rules = new Dictionary<string,char>();
-        static Dictionary<string, long> TemplateStats = new Dictionary<string, long>();
-        static Dictionary<string, long> StatsFinal = new Dictionary<string, long>();
+        static string? PTemplate;
+        static Dictionary<string, char> Rules           = new Dictionary<string, char>();
+        static Dictionary<string, long> TemplateStats   = new Dictionary<string, long>();
         public static void Main(string[] args)
         {
             ParsingInputData();
-            long nResOne = EncodePolymer(10);
-            long nResTwo = EncodePolymer(40);
 
-            Console.WriteLine("Part one: {0, 20:0}", nResOne);
-            Console.WriteLine("Part two: {0, 20:0}", nResTwo);
+            Console.WriteLine("Part one: {0, 20:0}", EncodePolymer(10));
+            Console.WriteLine("Part two: {0, 20:0}", EncodePolymer(40));
         }
         private static void AddValue(Dictionary<string, long> D, string key, long value)
         {
@@ -23,47 +21,43 @@
             else
                 D.Add(key, value);
         }
-        private static long CollectStats(Dictionary<string, long> Res)
-        {
-            StatsFinal.Clear();
-            foreach (var kvp in Res)
-            {
-                AddValue(StatsFinal, kvp.Key[0].ToString(), kvp.Value);
-                AddValue(StatsFinal, kvp.Key[1].ToString(), kvp.Value);
-            }
-            return StatsFinal.Values.Max() - StatsFinal.Values.Min();
-        }
         private static long EncodePolymer(int nSteps)
         {
+            Dictionary<string, long> StatsFinal  = new Dictionary<string, long>();
             Dictionary<string, long> ResultTempA = new Dictionary<string, long>();
             Dictionary<string, long> ResultTempB = new Dictionary<string, long>();
 
             foreach (var kvp in TemplateStats.Where(kvp=>kvp.Value > 0))
                     ResultTempA.Add(kvp.Key, kvp.Value);
 
-            long nRes = 0;
             for (int i = 0; i < nSteps; i++)
             {
                 ResultTempB.Clear();
-                foreach(KeyValuePair<string, long> pair in ResultTempA)
-                {
-                        string Key1 = pair.Key[0].ToString() + Rules[pair.Key];
-                        string Key2 = Rules[pair.Key] + pair.Key[1].ToString();
+                foreach(var pair in ResultTempA)
+                {   // CH -> B
+                    string Key1 = pair.Key[0].ToString() + Rules[pair.Key]; // Key1 = CB
+                    string Key2 = Rules[pair.Key] + pair.Key[1].ToString(); // Key2 = BH
 
-                        AddValue(ResultTempB, Key1, pair.Value);
-                        AddValue(ResultTempB, Key2, pair.Value);
+                    AddValue(ResultTempB, Key1, pair.Value);
+                    AddValue(ResultTempB, Key2, pair.Value);
                 }
+
                 ResultTempA.Clear();
                 foreach(var kvp in ResultTempB)
                     ResultTempA.Add(kvp.Key, kvp.Value);
             }
 
-            nRes = CollectStats(ResultTempA);
-            return (nRes + 1) / 2;
+            foreach (var kvp in ResultTempA)
+                AddValue(StatsFinal, kvp.Key[1].ToString(), kvp.Value);
+
+            // As we count the second character of each pair, we missed one instance of the very first char in the string, so adding +1 to the stats manually
+            // NNCB  => NN NC CB => N[N] N[C] C[B]   the [N] "N in square brackets" - counted only once, but there are two Ns in the  string
+            StatsFinal[PTemplate[0].ToString()]++;
+
+            return (StatsFinal.Values.Max() - StatsFinal.Values.Min());
         }
         private static void ParsingInputData()
         {
-            string? PTemplate;
             using (StreamReader file = new(filePath))
             {
                 PTemplate = file.ReadLine();
