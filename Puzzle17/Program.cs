cc75cb389c51289b6003e25a8e0dd121b5a6d4cf
Ilya Opyakin <i.opyakin@outlook.com>
2022-01-02 17:55:24 +0100

    Puzzle 17: Both partes completed

--- Puzzle17/Program.cs
+++ Puzzle17/Program.cs
@@ -2,28 +2,55 @@
 {
     public class Program
     {
-        // Answers for Data_p.txt  Part 1:      Part 2: 
-        static readonly string filePath = @".\..\..\..\Data_t.txt";
-        static List<int> InputData = new List<int>();
+        // Answers for Data_p.txt  Part 1: 2701     Part 2: 1070
+        // target area: x=281..311, y=-74..-54
+        static int Xmin = 281;
+        static int Xmax = 311;
+        static int Ymin = -74;
+        static int Ymax = -54;
         public static void Main(string[] args)
         {
-            ParsingInputData();
-            Console.WriteLine("Part one: {0, 10:0}", 1);
-            Console.WriteLine("Part one: {0, 10:0}", 2);
+            int nResOne = 0;
+            int nRestwo = 0;
+            int n = 0;
+            for (int x = 0; x <= Xmax; x++)
+                for (int y = -1000; y < 1000; y++)
+                    if (LaunchProbe(x, y, out n))
+                    {
+                        nRestwo++;
+                        if(n > nResOne) nResOne = n;
+                    }
+
+            Console.WriteLine("Part one: {0, 10:0}", nResOne);
+            Console.WriteLine("Part two: {0, 10:0}", nRestwo);
         }
-
-
-        private static void ParsingInputData()
+        private static bool LaunchProbe(int xVelocity, int yVelocity, out int highestY)
         {
-            using (StreamReader file = new(filePath))
-                while (!file.EndOfStream)
-                {
-                    string line = file.ReadLine();
-                    string[] parts = line.Split(",");
+            int x = 0;
+            int y = 0;
+            highestY = 0;
 
-                    foreach (string s in parts)
-                        InputData.Add(int.Parse(s));
-                }
+            while (x < Xmax && y > Ymin)
+            {
+                x += xVelocity;
+                y += yVelocity;
+
+                if (y > highestY) highestY = y;
+
+                if (xVelocity > 0) xVelocity--;
+                if (xVelocity < 0) xVelocity++;
+
+                yVelocity--;
+
+                if (IsBetween(x, Xmin, Xmax) && IsBetween(y, Ymin, Ymax))
+                    return true;
+            }
+
+            return false;
+        }
+        private static bool IsBetween(int numberToCheck, int Nmin, int nMax)
+        {
+            return (numberToCheck >= Nmin && numberToCheck <= nMax);
         }
     }
 }
