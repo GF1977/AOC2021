4128e18cf969070a718b926a3ef618eb7c27cd50
Ilya Opyakin <i.opyakin@outlook.com>
2022-01-06 23:14:21 +0100

    Started...

--- Puzzle19/Program.cs
+++ Puzzle19/Program.cs
@@ -1,13 +1,89 @@
 ﻿namespace MyApp
 {
+    public struct Coordinates
+    {
+        public int x { get; }
+        public int y { get; }
+        public int z { get; }
+        public Coordinates(int x, int y, int z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public static List<Coordinates> CompareTwoCoordinates(Coordinates A, Coordinates B)
+        {
+             List<Coordinates> result = new List<Coordinates>();
+            result.Add(new(A.x + B.x, A.y + B.y, A.z + B.z));
+            result.Add(new(A.x + B.x, A.y + B.y, A.z - B.z));
+            result.Add(new(A.x + B.x, A.y - B.y, A.z + B.z));
+            result.Add(new(A.x + B.x, A.y - B.y, A.z - B.z));
+            
+            result.Add(new(A.x - B.x, A.y + B.y, A.z + B.z));
+            result.Add(new(A.x - B.x, A.y + B.y, A.z - B.z));
+            result.Add(new(A.x - B.x, A.y - B.y, A.z + B.z));
+            result.Add(new(A.x - B.x, A.y - B.y, A.z - B.z));
+
+            return result;
+        }
+
+        public static Coordinates operator -(Coordinates A, Coordinates B)
+        {
+            return new Coordinates(A.x - B.x, A.y - B.y, A.z - B.z);
+        }
+
+        public static Coordinates operator +(Coordinates A, Coordinates B)
+        {
+            return new Coordinates(A.x + B.x, A.y + B.y, A.z + B.z);
+        }
+    }
+    public class Scanner
+    {
+        private int id { get; }
+        public List<Coordinates> coordinates = new List<Coordinates>();
+
+        public Scanner(int id)
+        {
+            this.id = id;
+        }
+
+        public List<Coordinates> CompareCoordinates(Scanner Target)
+        {
+            List<Coordinates> LDelta = new List<Coordinates>();
+
+            foreach (Coordinates crdScannerA in this.coordinates)
+                foreach (Coordinates crdScannerB in Target.coordinates)
+                {
+                    //Coordinates delta_crd = crdScannerA + crdScannerB;
+                    List<Coordinates> LDelta2 = Coordinates.CompareTwoCoordinates(crdScannerA, crdScannerB);
+                    LDelta.AddRange(LDelta2);
+                }
+            
+            return LDelta;
+        }
+
+    }
     public class Program
     {
         // Answers for Data_p.txt  Part 1:      Part 2: 
         static readonly string filePath = @".\..\..\..\Data_t.txt";
-        static List<int> InputData = new List<int>();
+        static List<Scanner> scanners = new List<Scanner>();
         public static void Main(string[] args)
         {
             ParsingInputData();
+            List<Coordinates> LDelta = new List<Coordinates>();
+            for (int i = 0; i < scanners.Count; i++)
+                for (int k = i+1; k < scanners.Count; k++)
+                {
+                    LDelta.AddRange(scanners[i].CompareCoordinates(scanners[k]));
+                }
+
+            var query = LDelta.GroupBy(x => x).Where(g => g.Count() >= 12).Select(y => y).ToList();
+
+            foreach (Coordinates crd in LDelta)
+                Console.WriteLine("x={0}  y={1}  z={2}", crd.x, crd.y, crd.z);
+            
             Console.WriteLine("Part one: {0, 10:0}", 1);
             Console.WriteLine("Part one: {0, 10:0}", 2);
         }
@@ -15,15 +91,29 @@
 
         private static void ParsingInputData()
         {
+            int nScannerNum = 0;
+            Scanner sc = new Scanner(nScannerNum);
             using (StreamReader file = new(filePath))
                 while (!file.EndOfStream)
                 {
                     string line = file.ReadLine();
-                    string[] parts = line.Split(",");
+                    
+                    if (line.Contains("scanner"))
+                        continue;
 
-                    foreach (string s in parts)
-                        InputData.Add(int.Parse(s));
+                    if(line == "")
+                    {
+                        scanners.Add(sc);
+                        nScannerNum++;
+                        sc = new Scanner(nScannerNum);
+                        continue;
+                    }
+
+                    string[] parts = line.Split(",");
+                    Coordinates crd = new Coordinates(int.Parse(parts[0]), int.Parse(parts[1]), int.Parse(parts[2]));
+                    sc.coordinates.Add(crd);
                 }
+                scanners.Add(sc);
         }
     }
 }
