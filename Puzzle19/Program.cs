b6f2ebb96f1764961b2aa72257c59b85c8d90004
Ilya Opyakin <i.opyakin@outlook.com>
2022-01-18 11:05:05 +0100

    Small enhancement

--- Puzzle19/Program.cs
+++ Puzzle19/Program.cs
@@ -14,56 +14,46 @@
 
         public Coordinate Rotate(int n)
         {
-            int x = this.x;
-            int y = this.y;
-            int z = this.z;
-
-            if (n < 0 || n > 23) throw new ArgumentOutOfRangeException();
-
             if (n == 0) return new Coordinate(x, y, z);
-            if (n == 1) return new Coordinate(x, -y, -z);
-            if (n == 2) return new Coordinate(x, z, -y);
-            if (n == 3) return new Coordinate(x, -z, y);
-            if (n == 4) return new Coordinate(-x, y, -z);
-            if (n == 5) return new Coordinate(-x, -y, z);
-            if (n == 6) return new Coordinate(-x, z, y);
-            if (n == 7) return new Coordinate(-x, -z, -y);
+            if (n == 1) return new Coordinate(-x, -y, z);
+            if (n == 2) return new Coordinate(y, -x, z);
+            if (n == 3) return new Coordinate(-y, x, z);
 
-            if (n == 8) return new Coordinate(y, -x, z);
-            if (n == 9) return new Coordinate(y, z, x);
-            if (n == 10) return new Coordinate(y, -z, -x);
-            if (n == 11) return new Coordinate(-y, x, z);
-            if (n == 12) return new Coordinate(-y, -x, -z);
-            if (n == 13) return new Coordinate(-y, z, -x);
-            if (n == 14) return new Coordinate(-y, -z, x);
-            if (n == 15) return new Coordinate(z, y, -x);
+            if (n == 4) return new Coordinate(y, x, -z);
+            if (n == 5) return new Coordinate(-y, -x, -z);
+            if (n == 6) return new Coordinate(x, -y, -z);
+            if (n == 7) return new Coordinate(-x, y, -z);
+
+            if (n == 8) return new Coordinate(y, z, x);
+            if (n == 9) return new Coordinate(-y, -z, x);
+            if (n == 10) return new Coordinate(z, -y, x);
+            if (n == 11) return new Coordinate(-z, y, x);
+
+            if (n == 12) return new Coordinate(z, y, -x);
+            if (n == 13) return new Coordinate(-z, -y, -x);
+            if (n == 14) return new Coordinate(y, -z, -x);
+            if (n == 15) return new Coordinate(-y, z, -x);
 
             if (n == 16) return new Coordinate(z, x, y);
-            if (n == 17) return new Coordinate(z, -x, -y);
-            if (n == 18) return new Coordinate(-z, x, -y);
-            if (n == 19) return new Coordinate(-z, -x, y);
-            if (n == 20) return new Coordinate(-z, y, x);
-            if (n == 21) return new Coordinate(-z, -y, -x);
-            if (n == 22) return new Coordinate(y, x, -z);
-            if (n == 23) return new Coordinate(z, -y, x);
+            if (n == 17) return new Coordinate(-z, -x, y);
+            if (n == 18) return new Coordinate(x, -z, y);
+            if (n == 19) return new Coordinate(-x, z, y);
 
-            return new Coordinate(0, 0, 0);
-        }
-        public static Coordinate operator -(Coordinate A, Coordinate B)
-        {
-            return new Coordinate(A.x - B.x, A.y - B.y, A.z - B.z);
-        }
+            if (n == 20) return new Coordinate(x, z, -y);
+            if (n == 21) return new Coordinate(-x, -z, -y);
+            if (n == 22) return new Coordinate(z, -x, -y);
+            if (n == 23) return new Coordinate(-z, x, -y);
 
-        public static Coordinate operator +(Coordinate A, Coordinate B)
-        {
-            return new Coordinate(A.x + B.x, A.y + B.y, A.z + B.z);
+            throw new ArgumentOutOfRangeException();
         }
+        public static Coordinate operator -(Coordinate A, Coordinate B) => (new Coordinate(A.x - B.x, A.y - B.y, A.z - B.z));
+        public static Coordinate operator +(Coordinate A, Coordinate B) => (new Coordinate(A.x + B.x, A.y + B.y, A.z + B.z));
     }
     public class Scanner
     {
         public List<Coordinate> beacons_Coordinate = new List<Coordinate>();
 
-        public bool CompareCoordinate(Scanner Target, int nSRotation, out Coordinate key)
+        public bool CompareCoordinates(Scanner Target, int nSRotation, out Coordinate key)
         {
             key = new Coordinate();
             List<Coordinate> LDelta = new List<Coordinate>();
@@ -88,9 +78,11 @@
     }
     public class Program
     {
-        // Answers for Data_p.txt               Part 1: 313     Part 2: 
-        // Answers for Data_p_alternative.txt   Part 1: 372     Part 2: 
+        // Answers for Data_t.txt    Part 1:  79     Part 2: 
 
+        // Answers for Data_p0.txt   Part 1: 313     Part 2: 
+        // Answers for Data_p1.txt   Part 1: 372     Part 2: 
+        // Answers for Data_p2.txt   Part 1: 440     Part 2: 
         static List<Scanner> scanners = new List<Scanner>();
 
         public static void Main(string[] args)
@@ -101,17 +93,20 @@
             Console.WriteLine("Part one: {0, 10:0}", nResOne);
 
             scanners.Clear();
-            ParsingInputData(@".\..\..\..\Data_p.txt");
-             nResOne = SolvePartOne();
+            ParsingInputData(@".\..\..\..\Data_p0.txt");
+            nResOne = SolvePartOne();
+            Console.WriteLine("Part one: {0, 10:0}", nResOne);
+
+
+            scanners.Clear();
+            ParsingInputData(@".\..\..\..\Data_p1.txt");
+            nResOne = SolvePartOne();
             Console.WriteLine("Part one: {0, 10:0}", nResOne);
 
             scanners.Clear();
-            ParsingInputData(@".\..\..\..\Data_p_alternative.txt");
-             nResOne = SolvePartOne();
+            ParsingInputData(@".\..\..\..\Data_p2.txt");
+            nResOne = SolvePartOne();
             Console.WriteLine("Part one: {0, 10:0}", nResOne);
-
-
-
 
             //Console.WriteLine("Part one: {0, 10:0}", nResTwo);
         }
@@ -123,7 +118,7 @@
                 for (int k = 1; k < scanners.Count; k++)
                     for (int nSRotation = 0; nSRotation < 24; nSRotation++)
                     {
-                        bool res = scanners[0].CompareCoordinate(scanners[k], nSRotation, out Coordinate key);
+                        bool res = scanners[0].CompareCoordinates(scanners[k], nSRotation, out Coordinate key);
                         if (res)
                         {
                             foreach (Coordinate c in scanners[k].beacons_Coordinate)
