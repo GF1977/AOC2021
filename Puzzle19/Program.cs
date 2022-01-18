54e9da1f568ca7fb42bc750dac1c018962884bb8
Ilya Opyakin <i.opyakin@outlook.com>
2022-01-18 13:15:58 +0100

    Day 19 completed both parts!

--- Puzzle19/Program.cs
+++ Puzzle19/Program.cs
@@ -1,140 +1,126 @@
 ﻿namespace MyApp
 {
-    public struct Coordinate
+    public struct Crd
     {
         public int x { get; }
         public int y { get; }
         public int z { get; }
-        public Coordinate(int x, int y, int z)
+        public Crd(int x, int y, int z)
         {
             this.x = x;
             this.y = y;
             this.z = z;
         }
 
-        public Coordinate Rotate(int n)
+        public Crd Rotate(int n)
         {
-            if (n == 0) return new Coordinate(x, y, z);
-            if (n == 1) return new Coordinate(-x, -y, z);
-            if (n == 2) return new Coordinate(y, -x, z);
-            if (n == 3) return new Coordinate(-y, x, z);
+            if (n ==  0) return new Crd(x, y, z);
+            if (n ==  1) return new Crd(-x, -y, z);
+            if (n ==  2) return new Crd(y, -x, z);
+            if (n ==  3) return new Crd(-y, x, z);
 
-            if (n == 4) return new Coordinate(y, x, -z);
-            if (n == 5) return new Coordinate(-y, -x, -z);
-            if (n == 6) return new Coordinate(x, -y, -z);
-            if (n == 7) return new Coordinate(-x, y, -z);
+            if (n ==  4) return new Crd(y, x, -z);
+            if (n ==  5) return new Crd(-y, -x, -z);
+            if (n ==  6) return new Crd(x, -y, -z);
+            if (n ==  7) return new Crd(-x, y, -z);
 
-            if (n == 8) return new Coordinate(y, z, x);
-            if (n == 9) return new Coordinate(-y, -z, x);
-            if (n == 10) return new Coordinate(z, -y, x);
-            if (n == 11) return new Coordinate(-z, y, x);
+            if (n ==  8) return new Crd(y, z, x);
+            if (n ==  9) return new Crd(-y, -z, x);
+            if (n == 10) return new Crd(z, -y, x);
+            if (n == 11) return new Crd(-z, y, x);
 
-            if (n == 12) return new Coordinate(z, y, -x);
-            if (n == 13) return new Coordinate(-z, -y, -x);
-            if (n == 14) return new Coordinate(y, -z, -x);
-            if (n == 15) return new Coordinate(-y, z, -x);
+            if (n == 12) return new Crd(z, y, -x);
+            if (n == 13) return new Crd(-z, -y, -x);
+            if (n == 14) return new Crd(y, -z, -x);
+            if (n == 15) return new Crd(-y, z, -x);
 
-            if (n == 16) return new Coordinate(z, x, y);
-            if (n == 17) return new Coordinate(-z, -x, y);
-            if (n == 18) return new Coordinate(x, -z, y);
-            if (n == 19) return new Coordinate(-x, z, y);
+            if (n == 16) return new Crd(z, x, y);
+            if (n == 17) return new Crd(-z, -x, y);
+            if (n == 18) return new Crd(x, -z, y);
+            if (n == 19) return new Crd(-x, z, y);
 
-            if (n == 20) return new Coordinate(x, z, -y);
-            if (n == 21) return new Coordinate(-x, -z, -y);
-            if (n == 22) return new Coordinate(z, -x, -y);
-            if (n == 23) return new Coordinate(-z, x, -y);
+            if (n == 20) return new Crd(x, z, -y);
+            if (n == 21) return new Crd(-x, -z, -y);
+            if (n == 22) return new Crd(z, -x, -y);
+            if (n == 23) return new Crd(-z, x, -y);
 
             throw new ArgumentOutOfRangeException();
         }
-        public static Coordinate operator -(Coordinate A, Coordinate B) => (new Coordinate(A.x - B.x, A.y - B.y, A.z - B.z));
-        public static Coordinate operator +(Coordinate A, Coordinate B) => (new Coordinate(A.x + B.x, A.y + B.y, A.z + B.z));
+        public static Crd operator -(Crd A, Crd B) => (new Crd(A.x - B.x, A.y - B.y, A.z - B.z));
+        public static Crd operator +(Crd A, Crd B) => (new Crd(A.x + B.x, A.y + B.y, A.z + B.z));
+        public int ManhattanDistance(Crd B) => Math.Abs(x - B.x) + Math.Abs(y - B.y) + Math.Abs(z - B.z);
     }
     public class Scanner
     {
-        public List<Coordinate> beacons_Coordinate = new List<Coordinate>();
-
-        public bool CompareCoordinates(Scanner Target, int nSRotation, out Coordinate key)
+        public List<Crd> beacons_coordinates = new List<Crd>();
+        public bool FindOverlappingScanner(Scanner Target, int nSRotation, out Crd relativeDistance )
         {
-            key = new Coordinate();
-            List<Coordinate> LDelta = new List<Coordinate>();
+            List<Crd> Overlapping = new List<Crd>();
 
-            foreach (Coordinate beaconA in this.beacons_Coordinate)
-                foreach (Coordinate beaconB in Target.beacons_Coordinate)
-                    LDelta.Add(beaconA - beaconB.Rotate(nSRotation));
+            foreach (Crd beaconA in this.beacons_coordinates)
+                foreach (Crd beaconB in Target.beacons_coordinates)
+                {
+                    Crd newCrd = beaconA - beaconB.Rotate(nSRotation);
+                    Overlapping.Add(newCrd);
+                }
 
-             
-
-            var query = LDelta.GroupBy(x => x).Where(g => g.Count() >= 12).Select(y => y).ToList();
-
+            var query = Overlapping.GroupBy(x => x).Where(g => g.Count() >= 12).Select(y => y).ToList();
+            relativeDistance = new Crd();
             if (query.Count() > 0)
             {
-                key = query[0].Key;
+                relativeDistance = query[0].Key;
                 return true;
             }
 
             return false;
         }
-
     }
     public class Program
     {
-        // Answers for Data_t.txt    Part 1:  79     Part 2: 
+        // Answers for Data_t.txt    Part 1:  79     Part 2  3621: 
+        // Answers for Data_p0.txt   Part 1: 313     Part 2 10656: 
 
-        // Answers for Data_p0.txt   Part 1: 313     Part 2: 
-        // Answers for Data_p1.txt   Part 1: 372     Part 2: 
-        // Answers for Data_p2.txt   Part 1: 440     Part 2: 
         static List<Scanner> scanners = new List<Scanner>();
-
+        static List<Crd> scannersCoordinates = new List<Crd>();
         public static void Main(string[] args)
         {
-            scanners.Clear();
-            ParsingInputData(@".\..\..\..\Data_t.txt");
-            int nResOne = SolvePartOne();
-            Console.WriteLine("Part one: {0, 10:0}", nResOne);
-
-            scanners.Clear();
             ParsingInputData(@".\..\..\..\Data_p0.txt");
-            nResOne = SolvePartOne();
-            Console.WriteLine("Part one: {0, 10:0}", nResOne);
+            Console.WriteLine("Part one: {0, 10:0}", SolvePartOne());
+            Console.WriteLine("Part two: {0, 10:0}", SolvePartTwo());
 
-
-            scanners.Clear();
-            ParsingInputData(@".\..\..\..\Data_p1.txt");
-            nResOne = SolvePartOne();
-            Console.WriteLine("Part one: {0, 10:0}", nResOne);
-
-            scanners.Clear();
-            ParsingInputData(@".\..\..\..\Data_p2.txt");
-            nResOne = SolvePartOne();
-            Console.WriteLine("Part one: {0, 10:0}", nResOne);
-
-            //Console.WriteLine("Part one: {0, 10:0}", nResTwo);
         }
-
         private static int SolvePartOne()
         {
+            Scanner S0 = scanners[0];
             while (scanners.Count != 1)
-            {
                 for (int k = 1; k < scanners.Count; k++)
                     for (int nSRotation = 0; nSRotation < 24; nSRotation++)
                     {
-                        bool res = scanners[0].CompareCoordinates(scanners[k], nSRotation, out Coordinate key);
-                        if (res)
+                        bool found = S0.FindOverlappingScanner(scanners[k], nSRotation, out Crd relativeDistance);
+                        if (found)
                         {
-                            foreach (Coordinate c in scanners[k].beacons_Coordinate)
-                            {
-                                Coordinate newC = c.Rotate(nSRotation) + key;
-                                scanners[0].beacons_Coordinate.Add(newC);
-                            }
+                            foreach (Crd b_crd in scanners[k].beacons_coordinates)
+                                S0.beacons_coordinates.Add(b_crd.Rotate(nSRotation) + relativeDistance);
+
+                            scannersCoordinates.Add(relativeDistance);
                             scanners.RemoveAt(k);
                             break;
                         }
                     }
-            }
-
-            return scanners[0].beacons_Coordinate.Select(c => c).Distinct().ToList().Count();
+            return S0.beacons_coordinates.Select(c => c).Distinct().ToList().Count();
         }
-
+        private static int SolvePartTwo()
+        {
+            int nMax = 0;
+            for (int i = 0; i < scannersCoordinates.Count; i++)
+                for (int k = i+1; k < scannersCoordinates.Count; k++)
+                {
+                    int n = scannersCoordinates[i].ManhattanDistance(scannersCoordinates[k]);
+                    if (n > nMax)
+                        nMax = n;
+                }
+            return nMax;
+        }
         private static void ParsingInputData(string filePath)
         {
             Scanner sc = new Scanner();
@@ -155,8 +141,8 @@
                     }
 
                     string[] parts = line.Split(",");
-                    Coordinate crd = new Coordinate(int.Parse(parts[0]), int.Parse(parts[1]), int.Parse(parts[2]));
-                    sc.beacons_Coordinate.Add(crd);
+                    Crd crd = new Crd(int.Parse(parts[0]), int.Parse(parts[1]), int.Parse(parts[2]));
+                    sc.beacons_coordinates.Add(crd);
                 }
             scanners.Add(sc);
         }
