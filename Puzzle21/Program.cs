2895c1268213b996986cb378f65af014db2c1d24
Ilya Opyakin <i.opyakin@outlook.com>
2022-01-29 22:55:13 +0100

    Puzzle 21: Part one - refactored

--- Puzzle21/Program.cs
+++ Puzzle21/Program.cs
@@ -2,87 +2,49 @@
 {
     public class Program
     {
-        // Answers for Data_p.txt  Part 1:      Part 2: 
-        static readonly string filePath = @".\..\..\..\Data_p.txt";
-        static int[] nPlayersPosition = { 0, 0 };
-        static int[] nPlayersScore = { 0, 0 };
-
-
-        static int nDiceLastNumber = 0;
+        // Answers for Data_p.txt  Part 1: 920580     Part 2: 
+        static readonly string filePath = @".\..\..\..\Data_t.txt";
         static int nDiceRollsNumber = 0;
-
         public static void Main(string[] args)
         {
-            ParsingInputData();
-            //for(int x = 0 ; x < 310; x++)
-            //{
-            //    Console.WriteLine(RollDice());
-            //}
+            (int Position, int Score) PlayerOne = (0, 0);
+            (int Position, int Score) PlayerTwo = (0, 0);
 
-            //nPlayersScore[0] < 1000 && nPlayersScore[1] < 1000
+            ParsingInputData(out PlayerOne.Position, out PlayerTwo.Position);
+
             while (true)
             {
-                if (!PlayTheGame())
+                PlayerOne = PlayRound(PlayerOne);
+                if (PlayerOne.Score >= 1000)
+                    break;
+
+                PlayerTwo = PlayRound(PlayerTwo);
+                if (PlayerTwo.Score >= 1000)
                     break;
             }
-            
-            int nLoserScore = Math.Min(nPlayersScore[0], nPlayersScore[1]); 
 
+            int nLowerScore = Math.Min(PlayerOne.Score, PlayerTwo.Score);
 
-            Console.WriteLine("nLoserScore:         {0, 10:0}", nLoserScore);
+            Console.WriteLine("nLoserScore:         {0, 10:0}", nLowerScore);
             Console.WriteLine("nDiceRollsNumber:    {0, 10:0}", nDiceRollsNumber);
-            Console.WriteLine("Part one:            {0, 10:0}", nLoserScore*nDiceRollsNumber);
+            Console.WriteLine("Part one:            {0, 10:0}", nLowerScore * nDiceRollsNumber);
         }
-
-        private static bool PlayTheGame()
+        private static (int,int) PlayRound((int Position, int Score) Player)
         {
-            bool bRes = false;
+            nDiceRollsNumber += 3;
+            int RollDice = 3 * ((nDiceRollsNumber - 1) % 100);
 
-            for (int nPlayer = 0; nPlayer < 2; nPlayer++)
-            {
-                string Roll;
-                nPlayersPosition[nPlayer] = (nPlayersPosition[nPlayer] + RollDice(out Roll)) % 10;
-                if (nPlayersPosition[nPlayer] == 0)
-                    nPlayersPosition[nPlayer] = 10;
-                nPlayersScore[nPlayer] += nPlayersPosition[nPlayer];
-                Console.WriteLine("Player {0} rolls {1} and moves to space {2} for a total score of {3}.", nPlayer + 1, Roll, nPlayersPosition[nPlayer], nPlayersScore[nPlayer]);
-                if (nPlayersScore[0] >= 1000 || nPlayersScore[1] >= 1000)
-                {
-                    return false;
-                }
-            }
-            
-           return true;
+            Player.Position = (Player.Position - 1 + RollDice) % 10 + 1;
+            Player.Score += Player.Position;
+
+            return Player;
         }
-
-        private static int RollDice(out string Roll)
-        {
-            Roll = string.Empty;
-            for (int i = 0; i<3; i++)
-            {
-                nDiceLastNumber = (nDiceLastNumber + 1) % 100;
-                if(nDiceLastNumber == 0) nDiceLastNumber = 100;
-                
-                Roll += " " + nDiceLastNumber.ToString();
-                nDiceRollsNumber++;
-            }
-            //Roll = (nDiceLastNumber - 2).ToString() + "+" + (nDiceLastNumber - 1).ToString() + "+" + (nDiceLastNumber).ToString();
-
-            return nDiceLastNumber*3 - 3;
-
-        }
-
-        private static void ParsingInputData()
+        private static void ParsingInputData(out int P1Position, out int P2Position)
         {
             StreamReader file = new(filePath);
-            string line = file.ReadLine();
-            string[] parts = line.Split("position: ");
-            nPlayersPosition[0] = int.Parse(parts[1]);
-
-            line = file.ReadLine();
-            parts = line.Split("position: ");
-            nPlayersPosition[1] = int.Parse(parts[1]);
-
+            string[] res = file.ReadToEnd().Split("\r\n");
+            P1Position = int.Parse(res[0].Split(": ")[1]);
+            P2Position = int.Parse(res[1].Split(": ")[1]);
         }
     }
 }
