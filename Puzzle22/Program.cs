3e88815d283a98d74a63e970444197a8ebae7a36
Ilya Opyakin <i.opyakin@outlook.com>
2022-01-30 22:46:24 +0100

    Puzzle22: small refactoring

--- Puzzle22/Program.cs
+++ Puzzle22/Program.cs
@@ -6,65 +6,56 @@
         public  (int Start, int End) X;
         public  (int Start, int End) Y;
         public  (int Start, int End) Z;
-
         public bool isValid()
         {
-            if(X.Start >= -50 && X.End <= 50 && Y.Start >= -50 && Y.End <= 50 && Z.Start >= -50 && Z.End <= 50)
-                return true;
-
-            return false;
+            return (X.Start >= -50 && X.End <= 50 && Y.Start >= -50 && Y.End <= 50 && Z.Start >= -50 && Z.End <= 50);
         }
     }
-
-
     public class Program
     {
-        // Answers for Data_p.txt  Part 1:      Part 2: 
+        // Answers for Data_p.txt  Part 1: 570915     Part 2: 
         static readonly string filePath = @".\..\..\..\Data_p.txt";
         
         static Dictionary<string,bool> Cuboids = new Dictionary<string,bool>();
-
         static List<Command> commands = new List<Command>();
-
         public static void Main(string[] args)
         {
             ParsingInputData();
             Console.WriteLine("Part one: {0, 10:0}", PartOne());
             Console.WriteLine("Part one: {0, 10:0}", 2);
         }
-
         private static int PartOne()
         {
             int nRes = 0;
             foreach(Command cmd in commands)
-            {
-                TurnOnCube(cmd);
-            }
-
-
-            foreach(var Cuboid in Cuboids)
-                if(Cuboid.Value)
-                    nRes++;
+                if (cmd.isValid())
+                    nRes += TurnOnCube(cmd);
 
             return nRes;
         }
-
-        private static void TurnOnCube(Command cmd)
+        private static int TurnOnCube(Command cmd)
         {
+            int nRes = 0;
             for(int x = cmd.X.Start; x <= cmd.X.End; x++)
                 for (int y = cmd.Y.Start; y <= cmd.Y.End; y++)
                     for (int z = cmd.Z.Start; z <= cmd.Z.End; z++)
                     {
                         string key = x.ToString() + ":" + y.ToString() + ":" + z.ToString();
                         Cuboids.TryAdd(key, false);
-                        if(cmd.cmd == "on")
+                        if (cmd.cmd == "on" && Cuboids[key] == false)
+                        {
                             Cuboids[key] = true;
-                        if (cmd.cmd == "off")
+                            nRes++;
+                        }
+                        if (cmd.cmd == "off" && Cuboids[key] == true)
+                        {
                             Cuboids[key] = false;
-
+                            nRes--;
+                        }
                     }
-        }
 
+            return nRes;
+        }
         private static void ParsingInputData()
         {
             using (StreamReader file = new(filePath))
@@ -86,8 +77,7 @@
                     StartEnd = parts[2].Split("=")[1].Split("..");
                     command.Z = (int.Parse(StartEnd[0]), int.Parse(StartEnd[1]));
 
-                    if(command.isValid())
-                        commands.Add(command);  
+                    commands.Add(command);  
                 }
         }
     }
