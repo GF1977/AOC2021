4f83de7a4f250fa5060111c5958b94b013689bce
Ilya Opyakin <i.opyakin@outlook.com>
2022-02-18 14:19:24 +0100

    Parsing + test + prod cases.
    Still problem with the second Data_pIO.txt case

--- Puzzle23/Program.cs
+++ Puzzle23/Program.cs
@@ -14,30 +14,20 @@ namespace MyApp
         public Dictionary<int, List<int>> Paths = new Dictionary<int, List<int>>();
         public Room()
         {
-            this.Id = IDcounter++;
-            this.isOccupiedBy = "";
-            this.OpenFor = "ABCD";
-            this.isVisited = false;
+            Id = IDcounter++;
+            isOccupiedBy = "";
+            OpenFor = "ABCD";
+            isVisited = false;
 
-            if (Id == 1) Connections = new List<int>() { 2 };
-            if (Id == 2) Connections = new List<int>() { 1, 16 };
-            if (Id == 3) Connections = new List<int>() { 16, 17 };
-            if (Id == 4) Connections = new List<int>() { 17, 18 };
-            if (Id == 5) Connections = new List<int>() { 18, 19 };
-            if (Id == 6) Connections = new List<int>() { 19, 7 };
-            if (Id == 7) Connections = new List<int>() { 6 };
-            if (Id == 8) Connections = new List<int>() { 12, 16 };
-            if (Id == 9) Connections = new List<int>() { 17, 13 };
-            if (Id == 10) Connections = new List<int>() { 18, 14 };
-            if (Id == 11) Connections = new List<int>() { 19, 15 };
-            if (Id == 12) Connections = new List<int>() { 8 };
-            if (Id == 13) Connections = new List<int>() { 9 };
-            if (Id == 14) Connections = new List<int>() { 10 };
-            if (Id == 15) Connections = new List<int>() { 11 };
-            if (Id == 16) Connections = new List<int>() { 2, 3, 8 };
-            if (Id == 17) Connections = new List<int>() { 3, 4, 9 };
-            if (Id == 18) Connections = new List<int>() { 4, 5, 10 };
-            if (Id == 19) Connections = new List<int>() { 5, 6, 11 };
+            Connections = new List<int>();
+
+            if (Id >= 11)
+            {
+                if (Id % 4 == 3) OpenFor = "A";
+                if (Id % 4 == 0) OpenFor = "B";
+                if (Id % 4 == 1) OpenFor = "C";
+                if (Id % 4 == 2) OpenFor = "D";
+            }
 
         }
         public object Clone()
@@ -60,18 +50,15 @@ namespace MyApp
         public int MoveCount { get; set; }
         public int MoveCost { get; set; } // 1 - 1000
         public int RoomId { get; set; }
-        // public bool JustMoved { get; set; }
         public bool ImHome { get; set; }
 
         public Shrimp(string Type)
         {
             this.Id = IDcounter++;
             this.ImHome = false;
-            //this.JustMoved = false;
             this.Type = Type;
             this.MoveCount = 0;
             this.RoomId = RoomId;
-
 
             this.MoveCost = (Type == "A") ? 1 : (Type == "B") ? 10 : (Type == "C") ? 100 : (Type == "D") ? 1000 : -1;
         }
@@ -80,68 +67,47 @@ namespace MyApp
             return this.MemberwiseClone();
         }
 
-        public void CheckHome(List<Room> Rooms)
+        public void AmIHome(List<Room> Rooms)
         {
-            if (Type == "A")
-            {
-                if ((RoomId == 12) || (RoomId == 8 && Rooms[12].isOccupiedBy == "A"))
-                    ImHome = true;
+            if (Type == Rooms[RoomId].OpenFor && Rooms[RoomId].Connections.Count == 1)
+                ImHome = true;
 
-            }
-            if (Type == "B")
-            {
-                if ((RoomId == 13) || (RoomId == 9 && Rooms[13].isOccupiedBy == "B"))
-                    ImHome = true;
 
-            }
+            Room RMax = Rooms.Find(r => r.Id == Rooms[RoomId].Connections.Max());
 
-            if (Type == "C")
-            {
-                if ((RoomId == 14) || (RoomId == 10 && Rooms[14].isOccupiedBy == "C"))
-                    ImHome = true;
-
-            }
-            if (Type == "D")
-            {
-                if ((RoomId == 15) || (RoomId == 11 && Rooms[15].isOccupiedBy == "D"))
-                    ImHome = true;
-
-            }
-
+            if (Type == Rooms[RoomId].OpenFor && RMax.isOccupiedBy == Type)
+                ImHome = true;
         }
+
     }
 
     public class Program
     {
-        // Answers for Data_p.txt  Part 1:      Part 2: 
+        // Answers for Data_p.txt  Part 1:  11516    Part 2: 
         static readonly string filePath = @".\..\..\..\Data_t.txt";
-        static List<int> InputData = new List<int>();
 
-        //static List<string> MovingOption = new List<string>();
-        //static List<string> MovingOptionBest = new List<string>();
-
-        static int nStepCountBest = int.MaxValue;
         static int nStepScoreBest = int.MaxValue;
         static int LastMovedShrimp = 0;
         static Stopwatch stopwatch;
 
-        static Dictionary<int, long> _memo = new() { { 0, 0 }, { 1, 1 } };
-
+        static List<Room> GlobalMap = new List<Room>();
+        static List<Shrimp> GlobalShrimps = new List<Shrimp>();
 
         public static void Main(string[] args)
         {
-            //ParsingInputData();
-            //RoomsEtalon = InitRooms();
-            List<Room> Rooms = InitRooms();
-            List<Shrimp> Shrimps = InitShrimps();
+            ParsingInputData();
+
+            List<Room> Rooms = GlobalMap;
+            List<Shrimp> Shrimps = GlobalShrimps;
+
+
 
             foreach (Shrimp shrimp in Shrimps)
-                shrimp.CheckHome(Rooms);
+                shrimp.AmIHome(Rooms);
 
-            int a, b;
 
-            foreach (Room RStart in Rooms.Where(r => r.Id != 0))
-                foreach (Room REnd in Rooms.Where(r => r.Id != 0))
+            foreach (Room RStart in Rooms)
+                foreach (Room REnd in Rooms)
                     if (RStart.Id != REnd.Id)
                     {
                         List<int> tmp = GetFullPath(RStart, REnd, Rooms);
@@ -156,130 +122,13 @@ namespace MyApp
             stopwatch.Start();
 
 
-            startMoving(Shrimps, Rooms, 0, 0, out a, out b);
+            startMoving(Shrimps, Rooms, 0);
 
-            //foreach (string S in MovingOptionBest)
-            //    Console.WriteLine(S);
 
             Console.WriteLine("Total time {0,8:0.000} Seconds", stopwatch.ElapsedMilliseconds / 1000.0);
 
-            Console.WriteLine("Part one: {0, 10:0}", nStepCountBest);
+            Console.WriteLine("Part one: {0, 10:0}", 0);
             Console.WriteLine("Part two: {0, 10:0}", nStepScoreBest);
-        }
-
-
-
-        private static List<Shrimp> InitShrimps()
-        {
-            List<Shrimp> Shrimps = new List<Shrimp>();
-
-            for (int i = 0; i < 8; i++)
-            {
-                string Type = string.Empty;
-                if (i == 0 || i == 1) Type = "A";
-                if (i == 2 || i == 3) Type = "B";
-                if (i == 4 || i == 5) Type = "C";
-                if (i == 6 || i == 7) Type = "D";
-                Shrimp S = new Shrimp(Type);
-                Shrimps.Add(S);
-            }
-
-
-            Shrimps[0].RoomId = 9;
-            Shrimps[1].RoomId = 13;
-            Shrimps[2].RoomId = 12;
-            Shrimps[3].RoomId = 10;
-            Shrimps[4].RoomId = 8;
-            Shrimps[5].RoomId = 15;
-            Shrimps[6].RoomId = 14;
-            Shrimps[7].RoomId = 11;
-
-
-            // TEST case from AOC
-            //Shrimps[0].RoomId = 12;
-            //Shrimps[1].RoomId = 15;
-            //Shrimps[2].RoomId = 8;
-            //Shrimps[3].RoomId = 10;
-            //Shrimps[4].RoomId = 9;
-            //Shrimps[5].RoomId = 14;
-            //Shrimps[6].RoomId = 13;
-            //Shrimps[7].RoomId = 11;
-
-
-            // the set below is for testing A changed with B
-
-            //Shrimps[0].RoomId = 9;
-            //Shrimps[1].RoomId = 12;
-            //Shrimps[2].RoomId = 8;
-            //Shrimps[3].RoomId = 13;
-            //Shrimps[4].RoomId = 10;
-            //Shrimps[5].RoomId = 14;
-            //Shrimps[6].RoomId = 11;
-            //Shrimps[7].RoomId = 15;
-
-
-            return Shrimps;
-        }
-
-        private static List<Room> InitRooms()
-        {
-            List<Room> Rooms = new List<Room>();
-
-            for (int i = 0; i < 20; i++)
-            {
-                Room R = new Room();
-                Rooms.Add(R);
-            }
-
-            Rooms[8].OpenFor = Rooms[12].OpenFor = "A";
-            Rooms[9].OpenFor = Rooms[13].OpenFor = "B";
-            Rooms[10].OpenFor = Rooms[14].OpenFor = "C";
-            Rooms[11].OpenFor = Rooms[15].OpenFor = "D";
-
-
-
-
-            Rooms[8].isOccupiedBy = "C";
-            Rooms[12].isOccupiedBy = "B";
-
-            Rooms[9].isOccupiedBy = "A";
-            Rooms[13].isOccupiedBy = "A";
-
-            Rooms[10].isOccupiedBy = "B";
-            Rooms[14].isOccupiedBy = "D";
-
-            Rooms[11].isOccupiedBy = "D";
-            Rooms[15].isOccupiedBy = "C";
-
-
-            // test case from AOC
-            //Rooms[8].isOccupiedBy = "B";
-            //Rooms[12].isOccupiedBy = "A";
-
-            //Rooms[9].isOccupiedBy = "C";
-            //Rooms[13].isOccupiedBy = "D";
-
-            //Rooms[10].isOccupiedBy = "B";
-            //Rooms[14].isOccupiedBy = "C";
-
-            //Rooms[11].isOccupiedBy = "D";
-            //Rooms[15].isOccupiedBy = "A";
-
-
-
-            // the set below is for testing A changed with B
-
-            //Rooms[9].isOccupiedBy = "A";
-            //Rooms[12].isOccupiedBy = "A";
-            //Rooms[8].isOccupiedBy = "B";
-            //Rooms[13].isOccupiedBy = "B";
-            //Rooms[10].isOccupiedBy = "C";
-            //Rooms[14].isOccupiedBy = "C";
-            //Rooms[11].isOccupiedBy = "D";
-            //Rooms[15].isOccupiedBy = "D";
-
-
-            return Rooms;
         }
 
         private static List<int> GetFullPath(Room rStart, Room Rend, List<Room> Rooms)
@@ -305,10 +154,14 @@ namespace MyApp
 
         private static bool IsRightDistination(Shrimp S, Room Rend, List<Room> Rooms)
         {
-            if (S.Type == "A" && (Rend.Id == 8 && Rooms[12].isOccupiedBy == "A") || Rend.Id == 12) return true;
-            if (S.Type == "B" && (Rend.Id == 9 && Rooms[13].isOccupiedBy == "B") || Rend.Id == 13) return true;
-            if (S.Type == "C" && (Rend.Id == 10 && Rooms[14].isOccupiedBy == "C") || Rend.Id == 14) return true;
-            if (S.Type == "D" && (Rend.Id == 11 && Rooms[15].isOccupiedBy == "D") || Rend.Id == 15) return true;
+            if (S.Type == Rend.OpenFor && Rend.Connections.Count == 1)
+                return true;
+
+
+            Room RMax = Rooms.Find(r => r.Id == Rend.Connections.Max());
+
+            if (S.Type == Rend.OpenFor && RMax.isOccupiedBy == S.Type)
+                return true;
 
             return false;
         }
@@ -317,8 +170,7 @@ namespace MyApp
             if (S.ImHome) return false;
             if (S.Id == LastMovedShrimp) return false;
 
-
-            if (toRoom == 0 || S.RoomId == toRoom || Rooms[toRoom].isOccupiedBy != "")
+            if (S.RoomId == toRoom || Rooms[toRoom].isOccupiedBy != "")
                 return false;
 
             if (Rooms[S.RoomId].OpenFor == "ABCD" && !IsRightDistination(S, Rooms[toRoom], Rooms))
@@ -338,8 +190,6 @@ namespace MyApp
 
         internal static string MoveTo(Shrimp S, int toRoom, List<Shrimp> Shrimps, List<Room> Rooms)
         {
-
-
             LastMovedShrimp = S.Id;
 
             Room Rstart = Rooms[S.RoomId];
@@ -350,7 +200,8 @@ namespace MyApp
             Rstart.isOccupiedBy = "";
             Rend.isOccupiedBy = S.Type;
 
-            if (Rend.OpenFor != "ABCD") S.ImHome = true;
+            if (Rend.OpenFor != "ABCD")
+                S.ImHome = true;
 
             S.RoomId = Rend.Id;
 
@@ -358,21 +209,14 @@ namespace MyApp
         }
 
 
-        private static int startMoving(List<Shrimp> Shrimps, List<Room> Rooms, int recStepCount, int recScore, out int outStepCount, out int outScore)
+        private static int startMoving(List<Shrimp> Shrimps, List<Room> Rooms, int recScore)
         {
-            outStepCount = 0;
-            outScore = 0;
 
             if (nStepScoreBest <= recScore)
                 return 1;
 
-
-            //int rDestination;
             if (isCorrectOrder(Shrimps))
             {
-                outStepCount = recStepCount;
-                outScore = recScore;
-
                 Console.WriteLine("Best score: {0}   - time {1,8:0.000} Seconds", recScore, stopwatch.ElapsedMilliseconds / 1000.0);
                 return 0;
             }
@@ -384,50 +228,30 @@ namespace MyApp
 
                 foreach (Room REnd in Rooms)
                 {
-                    //int res;
-
-
                     if (isMovable(S, REnd.Id, Rooms))
                     {
 
-                        int res = RStart.Paths[REnd.Id].Count();
-                        if (res > 0)
+                        List<Room> RoomsTmp = new List<Room>();
+                        foreach (Room RR in Rooms)
+                            RoomsTmp.Add((Room)RR.Clone());
+
+                        List<Shrimp> ShrimpsTmp = new List<Shrimp>();
+                        foreach (Shrimp SS in Shrimps)
+                            ShrimpsTmp.Add((Shrimp)SS.Clone());
+
+                        MoveTo(ShrimpsTmp[S.Id], REnd.Id, ShrimpsTmp, RoomsTmp);
+
+                        int recScoreTMP = recScore + RStart.Paths[REnd.Id].Count() * S.MoveCost;
+
+                        if (nStepScoreBest < recScoreTMP)
+                            return 1;
+
+                        if (startMoving(ShrimpsTmp, RoomsTmp, recScoreTMP) == 0)
                         {
+                            if (recScoreTMP < nStepScoreBest && recScoreTMP > 0)
+                                nStepScoreBest = recScoreTMP;
 
-                            List<Room> RoomsTmp = new List<Room>();
-                            foreach (Room RR in Rooms)
-                                RoomsTmp.Add((Room)RR.Clone());
-
-                            List<Shrimp> ShrimpsTmp = new List<Shrimp>();
-                            foreach (Shrimp SS in Shrimps)
-                                ShrimpsTmp.Add((Shrimp)SS.Clone());
-
-                            string output = MoveTo(ShrimpsTmp[S.Id], REnd.Id, ShrimpsTmp, RoomsTmp);
-
-                            //MovingOption.Add(output);
-
-
-                            int recStepCountTMP = recStepCount + res;
-                            int recScoreTMP = recScore + res * S.MoveCost;
-
-                            if (nStepScoreBest < recScoreTMP)
-                                return 1;
-
-                            if (startMoving(ShrimpsTmp, RoomsTmp, recStepCountTMP, recScoreTMP, out recStepCountTMP, out recScoreTMP) == 0)
-                            {
-
-                                if (recScoreTMP < nStepScoreBest && recScoreTMP > 0)
-                                {
-                                    nStepCountBest = recStepCountTMP;
-                                    nStepScoreBest = recScoreTMP;
-                                    //MovingOptionBest.Clear();
-                                    //MovingOptionBest.AddRange(MovingOption);
-                                }
-
-
-                                //MovingOption.Clear();
-                                return 1;
-                            }
+                            return 1;
                         }
                     }
                 }
@@ -448,15 +272,56 @@ namespace MyApp
 
         private static void ParsingInputData()
         {
+            int[,] map = new int[13, 5];
+            int X = 0, Y = 0;
             using (StreamReader file = new(filePath))
                 while (!file.EndOfStream)
                 {
                     string line = file.ReadLine();
-                    string[] parts = line.Split(",");
+                    foreach (char c in line)
+                    {
+                        if (c == '.' || c == 'A' || c == 'B' || c == 'C' || c == 'D')
+                        {
+                            Room R = new Room();
+                            map[X, Y] = 1000 + R.Id;
 
-                    foreach (string s in parts)
-                        InputData.Add(int.Parse(s));
+                            if (c != '.')
+                            {
+                                Shrimp S = new Shrimp(c.ToString());
+                                R.isOccupiedBy = S.Type;
+                                S.RoomId = R.Id;
+                                GlobalShrimps.Add(S);
+                            }
+
+                            GlobalMap.Add(R);
+                        }
+                        X++;
+                    }
+                    X = 0;
+                    Y++;
                 }
+
+
+            for (int x = 0; x < 13; x++)
+                for (int y = 0; y < 5; y++)
+                {
+                    if (map[x, y] >= 1000)
+                    {
+
+                        int RoomID = map[x, y] - 1000;
+                        Room R = GlobalMap.Find(r => r.Id == RoomID);
+
+                        if (map[x, y + 1] >= 1000)
+                            R.Connections.Add(map[x, y + 1] - 1000);
+                        if (map[x, y - 1] >= 1000)
+                            R.Connections.Add(map[x, y - 1] - 1000);
+                        if (map[x + 1, y] >= 1000)
+                            R.Connections.Add(map[x + 1, y] - 1000);
+                        if (map[x - 1, y] >= 1000)
+                            R.Connections.Add(map[x - 1, y] - 1000);
+                    }
+                }
+
         }
     }
 }
