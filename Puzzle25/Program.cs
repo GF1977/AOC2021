714c257cb4b86569cef32236f9363d3b062575a0
Ilya Opyakin <i.opyakin@outlook.com>
2022-03-19 23:08:08 +0100

    Solution 25 - sceleton

---
+++ Puzzle25/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace MyApp
+{    
+    public class Program
+    {
+        // Answers for Data_p.txt  Part 1:      Part 2: 
+        static char[,] map = new char[X, Y];
+        static int X, Y;
+        public static void Main(string[] args)
+        {
+            ParsingInputData(@".\..\..\..\Data_t.txt");
+
+            int res = SolveProblem(1);
+
+            Console.WriteLine("Part one: {0, 10:0}", res);
+            Console.WriteLine("Part one: {0, 10:0}", 0);
+        }
+
+
+        private static int SolveProblem(int v)
+        {
+            int nMoved = 1;
+            int nSteps = 0;
+
+            while (nMoved > 0)
+            {
+                nMoved = MoveCucumbers("East") + MoveCucumbers("South");
+                nSteps++;
+            }
+
+            return 0;
+        }
+
+        private static int MoveCucumbers(string v)
+        {
+            return 0;
+        }
+
+        private static void ParsingInputData(string filePath)
+        {
+            StreamReader file = new(filePath);
+            string[] Data = file.ReadToEnd().Split("\r\n");
+            
+            // Max Size of the region
+            Y = Data[0].Length;
+            X = Data.Length;
+
+            for (int x = 0; x < X; x++)
+                for (int y = 0; y < Y; y++)
+                {
+                    map[x,y] = Data[x][y];
+                }
+        }
+    }
+}
