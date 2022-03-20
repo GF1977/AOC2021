c34173828a72a0fdd5839a86509db70d2e7a1cf4
Ilya Opyakin <i.opyakin@outlook.com>
2022-03-20 16:56:52 +0100

    Completed! need to do small refactoring

--- Puzzle25/Program.cs
+++ Puzzle25/Program.cs
@@ -2,53 +2,116 @@
 {    
     public class Program
     {
-        // Answers for Data_p.txt  Part 1:      Part 2: 
-        static char[,] map = new char[X, Y];
+        // Answers for Data_p.txt  Part 1: 278       
+        static char[,] map;
+        static bool[,] FreeSlots;
         static int X, Y;
         public static void Main(string[] args)
         {
-            ParsingInputData(@".\..\..\..\Data_t.txt");
-
-            int res = SolveProblem(1);
-
-            Console.WriteLine("Part one: {0, 10:0}", res);
-            Console.WriteLine("Part one: {0, 10:0}", 0);
+            ParsingInputData(@".\..\..\..\Data_p.txt");
+            Console.WriteLine("Part one: {0, 10:0}", SolveProblem());
         }
-
-
-        private static int SolveProblem(int v)
+        private static int SolveProblem()
         {
-            int nMoved = 1;
+            bool bMoved = true;
             int nSteps = 0;
 
-            while (nMoved > 0)
+            while (bMoved)
             {
-                nMoved = MoveCucumbers("East") + MoveCucumbers("South");
+                bMoved = MoveCucumbersE("East");
+                bool b = MoveCucumbersS("South");
+                bMoved = bMoved || b;
                 nSteps++;
-            }
-
-            return 0;
+            } 
+            return nSteps;
         }
-
-        private static int MoveCucumbers(string v)
+        private static void CheckFreeSlots()
         {
-            return 0;
-        }
+            FreeSlots = new bool[Y, X];
 
+            for (int y = 0; y < Y; y++)
+                for (int x = 0; x < X; x++)
+                {
+                    if (map[y, x] == '.')
+                        FreeSlots[y, x] = true;
+                    else
+                        FreeSlots[y, x] = false;
+
+                    if (map[y, x] == 'E') map[y, x] = '>';
+                    if (map[y, x] == 'S') map[y, x] = 'v';
+                }
+        }
+        private static bool MoveCucumbersE(string v)
+        {
+            CheckFreeSlots();
+            bool bMoved = false;
+
+            for (int y = 0; y < Y; y++)
+                for (int x = 0; x < X; x++)
+                {
+                    if (map[y, x] == '>')
+                        if (x < X - 1)
+                        {
+                            if (FreeSlots[y,x+1])
+                            {
+                                map[y, x] = '.';
+                                map[y, x+1] = 'E';
+                                bMoved = true;
+                            }
+                        }
+                        else
+                        {
+                            if (FreeSlots[y,0])
+                            {
+                                map[y, x] = '.';
+                                map[y, 0] = 'E';
+                                bMoved = true;
+                            }
+                        }
+                }
+           return bMoved ;
+        }
+        private static bool MoveCucumbersS(string v)
+        {
+            CheckFreeSlots();
+            bool bMoved = false;
+
+            for (int y = 0; y < Y; y++)
+                for (int x = 0; x < X; x++)
+                {
+                    if (map[y, x] == 'v')
+                        if (y < Y - 1)
+                        {
+                            if (FreeSlots[y+1,x])
+                            {
+                                map[y, x] = '.';
+                                map[y+1, x] = 'S';
+                                bMoved = true;
+                            }
+                        }
+                        else
+                        {
+                             if (FreeSlots[0,x])
+                             {
+                                map[y, x] = '.';
+                                map[0, x] = 'S';
+                                bMoved = true;
+                            }
+                        }
+                }
+            return bMoved;
+        }
         private static void ParsingInputData(string filePath)
         {
             StreamReader file = new(filePath);
             string[] Data = file.ReadToEnd().Split("\r\n");
-            
-            // Max Size of the region
-            Y = Data[0].Length;
-            X = Data.Length;
+            X = Data[0].Length;
+            Y = Data.Length;
+            map = new char[Y, X];
 
-            for (int x = 0; x < X; x++)
-                for (int y = 0; y < Y; y++)
-                {
-                    map[x,y] = Data[x][y];
-                }
+            for (int y = 0; y < Y; y++)
+                for (int x = 0; x < X; x++)
+                    map[y,x] = Data[y][x];
         }
     }
 }
